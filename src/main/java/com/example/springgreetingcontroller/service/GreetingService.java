028dcbcbca45b916a11b1ac6643abed6ae2d306c
rksgr <r_kjha@yahoo.com>
2022-02-07 12:02:59 +0530

    Added: Service method

--- src/main/java/com/example/springgreetingcontroller/service/GreetingService.java
+++ src/main/java/com/example/springgreetingcontroller/service/GreetingService.java
@@ -1,2 +1,24 @@
-package com.example.springgreetingcontroller.service;public class GreetingService {
+package com.example.springgreetingcontroller.service;
+
+import com.example.springgreetingcontroller.entity.Greeting;
+import com.example.springgreetingcontroller.entity.User;
+import com.example.springgreetingcontroller.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class GreetingService implements IGreetingService {
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    @Override
+    public Greeting addGreeting(User user) {
+        String message = String.format(template, (user.toString().isEmpty()) ? "Hello, India" : user.toString());
+        return greetingRepository.save(new Greeting(counter.incrementAndGet(), message));
+    }
 }
