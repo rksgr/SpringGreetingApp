d5c9032de3fe7f108aa7cf746570b6f36aa4cdc1
rksgr <r_kjha@yahoo.com>
2022-02-07 12:00:20 +0530

    Added: User identity

--- src/main/java/com/example/springgreetingcontroller/entity/User.java
+++ src/main/java/com/example/springgreetingcontroller/entity/User.java
@@ -1,2 +1,18 @@
-package com.example.springgreetingcontroller.entity;public class User {
+package com.example.springgreetingcontroller.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// Data = Getter + Setter + tostring
+@Getter
+@Setter
+public class User {
+    private String firstName;
+    private String lastName;
+
+    public String toString() {
+        String str = (firstName != null) ? firstName + "" : "";
+        str += (lastName != null) ? lastName : "";
+        return str.trim();
+    }
 }
