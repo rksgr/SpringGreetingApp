962f6a2c165f266cc30319d4d17de39ea7012010
rksgr <r_kjha@yahoo.com>
2022-02-07 12:03:41 +0530

    Added: Service Interface

--- src/main/java/com/example/springgreetingcontroller/service/IGreetingService.java
+++ src/main/java/com/example/springgreetingcontroller/service/IGreetingService.java
@@ -1,2 +1,11 @@
-package com.example.springgreetingcontroller.service;public interface IGreetingService {
+package com.example.springgreetingcontroller.service;
+
+import com.example.springgreetingcontroller.entity.Greeting;
+import com.example.springgreetingcontroller.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface IGreetingService {
+
+    Greeting addGreeting(User user);
 }
